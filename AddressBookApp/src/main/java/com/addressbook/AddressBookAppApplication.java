ffbc8b1f58aa7c68573e4cb0bb5ae231f6dd7d1a
thakur-Yuvraj <yuvipatel224@gmail.com>
2025-02-28 13:58:51 +0530

    [Yuvraj Thakur] Add. Created address book project cater to REST api calls

---
+++ AddressBookApp/src/main/java/com/addressbook/AddressBookAppApplication.java
@@ -0,0 +1,14 @@
+package com.addressbook;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class AddressBookAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Spring app is running");
+	}
+}
